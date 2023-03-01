346b0539d8fa940087a694adaec3637a9482251d
outtasight1010 <126138474+outtasight1010@users.noreply.github.com>
2023-03-01 14:14:11 -0500

    Add files via upload
    #added Weapon class, main.py, Robot class

---
+++ robot.py
@@ -0,0 +1,10 @@
+
+class Robot:
+    def __init__(self):
+        self.name = "RX714"
+        self.health = 50
+        self.power_level = 100
+        self.weapon = Weapon()
+
+    def choose_weapon(self, weapons_list)
+        
---
+++ weapon.py
@@ -0,0 +1,7 @@
+
+import random
+weapons_list = ["hammer", "axe", "grenade"]
+class Weapon:
+    def __init__(self):
+        self.name = weapons_list
+        self.attack_power = 100
